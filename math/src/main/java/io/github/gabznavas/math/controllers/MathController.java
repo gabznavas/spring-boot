e2f39633111ca2fc791c3a3859d7b5befe26ecf8
navas <gabrielnavas@protonmail.com>
2025-02-23 23:05:14 -0300

    basic math

---
+++ math/src/main/java/io/github/gabznavas/math/controllers/MathController.java
@@ -0,0 +1,42 @@
+package io.github.gabznavas.math.controllers;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/math")
+public class MathController {
+
+    // http://localhost:8080/math/sum/3/5
+    @RequestMapping("/sum/{numberOne}/{numberTwo}")
+    public Double sum(
+            @PathVariable("numberOne")
+            String numberOne,
+            @PathVariable("numberTwo")
+            String numberTwo
+    ) throws Exception {
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new IllegalArgumentException();
+        }
+        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+    private Double convertToDouble(String strNumber) {
+        if(strNumber == null || strNumber.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        return Double.parseDouble(strNumber);
+    }
+
+    private boolean isNumeric(String strNumber) {
+        if(strNumber == null || strNumber.isEmpty()) {
+            return false;
+        }
+        String number = strNumber.replace(",", "."); // R$5,00 USD 5.0
+
+        // verifica se são números positivos e negativos e de 0 a 9 e depois do ponto se são de 0 a 9
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+    }
+}
